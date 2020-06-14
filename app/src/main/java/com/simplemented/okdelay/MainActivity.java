b31956285e5824505298181a45cb4439a020091c
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-13 23:41:09 -0700

    Migrate to AndroidX

--- app/src/main/java/com/simplemented/okdelay/MainActivity.java
+++ app/src/main/java/com/simplemented/okdelay/MainActivity.java
@@ -1,7 +1,7 @@
 package com.simplemented.okdelay;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
